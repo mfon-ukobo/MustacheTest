d8537836ff24ea47fc366fea1de9e0da5114431d
Mfon Ukobo <coache1234@gmail.com>
2024-05-15 11:20:54 +0100

    Add project files.

---
+++ MustacheTest/InvoiceGeneratorModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MustacheTest;
+public class InvoiceGeneratorModel
+{
+	public required Invoice Invoice { get; set; }
+	public required Company Company { get; set; }
+}
+
+
+public class Invoice
+{
+	public IEnumerable<InvoiceItem> Items { get; set; } = Enumerable.Empty<InvoiceItem>();
+}
+
+public class InvoiceItem
+{
+    [SetsRequiredMembers]
+    public InvoiceItem(string description, decimal amount)
+    {
+        Description = description;
+        Amount = amount;
+    }
+
+    public required string Description { get; set; }
+	public decimal Amount { get; set; } = 0;
+}
+
+public class Company(string name)
+{
+    public string Name { get; set; } = name;
+}
---
+++ MustacheTest/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Mustache;
+
+using MustacheTest;
+
+var htmlText = await File.ReadAllTextAsync("index.html");
+
+var htmlFormatCompiler = new HtmlFormatCompiler();
+Generator generator = htmlFormatCompiler.Compile(htmlText);
+
+var renderModel = new InvoiceGeneratorModel
+{
+	Invoice = new Invoice()
+	{
+		Items = [
+			new InvoiceItem("Applicaiton for some item", 500),
+			new InvoiceItem("Applicaiton for some other item", 4500),
+		]
+	},
+	Company = new Company("Maxfront Technologies")
+};
+
+var newHtml = generator.Render(renderModel);
+
+File.WriteAllText("generated.html", newHtml);
